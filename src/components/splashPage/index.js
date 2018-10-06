e7479d0213da1028713e075c8c4489e30a31dfc9
Adrian Rodriguez <adrianwritenow@gmail.com>
2018-10-06 18:00:41 -0400

    linked a new font style and  title on splash

--- src/components/splashPage/index.js
+++ src/components/splashPage/index.js
@@ -10,7 +10,10 @@ class SplashPage extends Component {
 
     return (
       <div classNameName="splashContainer">
-
+        <div className='titleContainer'>
+          <h1>DIGITAL</h1>
+          <h3>DUNGEON</h3>
+        </div>
         <img src='https://web.archive.org/web/20070305181137/http://www.geocities.com/hardcandygraphix/Logos/LogoStartButton.gif'/>
 
       </div>
