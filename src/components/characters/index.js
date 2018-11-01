9c490f3bb92a4b2eb3b6ae170ef68c54a3d4137b
Adrian Rodriguez <adrianwritenow@gmail.com>
2018-10-31 20:32:01 -0400

    add css to sections of characters and cast and placeholder

--- src/components/characters/index.js
+++ src/components/characters/index.js
@@ -10,6 +10,34 @@ class Characters extends Component {
         <h1>Characters</h1>
       </section>
 
+      <div className='characterCard'>
+        <h1>CHARACTER 1</h1>
+        <p>Digital Dungeon is a Dungeons and Dragons 5th edition actual-play
+          podcast that attempts to entertain through high-fantasy hi-jinx in the
+          digital world of Aoll. Join us as we hit the squared circle with wrestling
+          extarodinare Holy Hezrou, hit the books with the ever-studious mage-in-training
+          Rudy Cobblehead, and hit the courtroom with Gad, Tortle Attorney-at-Law!
+        </p>
+      </div>
+      <div className='characterCard'>
+        <h1>CHARACTER 1</h1>
+        <p>Digital Dungeon is a Dungeons and Dragons 5th edition actual-play
+          podcast that attempts to entertain through high-fantasy hi-jinx in the
+          digital world of Aoll. Join us as we hit the squared circle with wrestling
+          extarodinare Holy Hezrou, hit the books with the ever-studious mage-in-training
+          Rudy Cobblehead, and hit the courtroom with Gad, Tortle Attorney-at-Law!
+        </p>
+      </div>
+      <div className='characterCard'>
+        <h1>CHARACTER 1</h1>
+        <p>Digital Dungeon is a Dungeons and Dragons 5th edition actual-play
+          podcast that attempts to entertain through high-fantasy hi-jinx in the
+          digital world of Aoll. Join us as we hit the squared circle with wrestling
+          extarodinare Holy Hezrou, hit the books with the ever-studious mage-in-training
+          Rudy Cobblehead, and hit the courtroom with Gad, Tortle Attorney-at-Law!
+        </p>
+      </div>
+
       </div>
 
 
