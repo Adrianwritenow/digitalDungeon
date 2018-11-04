4cb6748dc422ddb0ddabb19ff96fefd25997d1c0
Adrian Rodriguez <adrianwritenow@gmail.com>
2018-11-04 16:51:09 -0500

    cards flip

--- src/components/characters/index.js
+++ src/components/characters/index.js
@@ -2,45 +2,14 @@ import React, { Component } from 'react';
 import '../../App.css';
 import "animate.css/animate.min.css";
 import ScrollAnimation from 'react-animate-on-scroll';
-import FlipCard from 'react-flipcard-2';
+import Flippy, { FrontSide, BackSide } from 'react-flippy';
+
+
 
 
 
 class Characters extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-    isFlipped:false
-    };
 
-    this.frontCard = React.createRef();
-
-
-      this.showFront = this.showFront.bind(this);
-      this.showBack = this.showBack.bind(this);
-      this.handleOnFlip = this.handleOnFlip.bind(this);
-
-  }
-
-
-
-  showBack() {
-    this.setState({
-      isFlipped: true
-    });
-  }
-
-  showFront() {
-    this.setState({
-      isFlipped: false
-    });
-  }
-
-  handleOnFlip(flipped) {
-   if (flipped) {
-     this.frontCard.current.focus();
-   }
- }
 
 
   render() {
@@ -56,42 +25,80 @@ class Characters extends Component {
           <div className='container2'>
           <ScrollAnimation delay={400} animateIn="rubberBand">
             <div className='slide' id='HH'>
-              <div className='slideText'>
-                <h1>Holy Hezrou</h1>
-                <p>Digital Dungeon is a Dungeons and Dragons 5th edition actual-play
-                  podcast that attempts to entertain through high-fantasy hi-jinx in the
-                  digital world of Aoll. Join us as we hit the squared circle with wrestling
-                  extarodinare Holy Hezrou, hit the books with the ever-studious mage-in-training
-                  Rudy Cobblehead, and hit the courtroom with Gad, Tortle Attorney-at-Law!
-                </p>
-              </div>
-                <img src='images/hezrouTitle.png' className='profilePic animated infinite bounce delay-2s"'/>
+            <Flippy
+                flipOnHover={false} // default false
+                flipOnClick={true} // default false
+                flipDirection="horizontal" // horizontal or vertical
+                ref={(r) => this.flippy = r} // to use toggle method like this.flippy.toggle()
+                // if you pass isFlipped prop component will be controlled component.
+                // and other props, which will go to div
+               /// these are optional style, it is not necessary
+            >
+
+            <FrontSide>
+                <img src='images/hezrouTitle.png' ref={this.backCard} className='profilePic animated infinite bounce delay-2s"' onClick={this.showBack}/>
+              </FrontSide>
+              <BackSide style={{ backgroundColor: '#175852'}}>
+                <div className='slideText' ref={this.frontCard} onClick={this.showFront}>
+                  <h1>HOLY HEZROU</h1>
+                  <p>Digital Dungeon is a Dungeons and Dragons 5th edition actual-play
+                    podcast that attempts to entertain through high-fantasy hi-jinx in the
+                    digital world of Aoll. Join us as we hit the squared circle with wrestling
+                    extarodinare Holy Hezrou, hit the books with the ever-studious mage-in-training
+                    Rudy Cobblehead, and hit the courtroom with Gad, Tortle Attorney-at-Law!
+                  </p>
+                </div>
+                </BackSide>
+              </Flippy>
             </div>
           </ScrollAnimation>
           <ScrollAnimation delay={400} animateIn="fadeIn" duration={3}>
             <div className='slide' id='RC'>
-              <div className='slideText'>
-                <h1>Rudy Cobblehead</h1>
-                <p>Digital Dungeon is a Dungeons and Dragons 5th edition actual-play
-                  podcast that attempts to entertain through high-fantasy hi-jinx in the
-                  digital world of Aoll. Join us as we hit the squared circle with wrestling
-                  extarodinare Holy Hezrou, hit the books with the ever-studious mage-in-training
-                  Rudy Cobblehead, and hit the courtroom with Gad, Tortle Attorney-at-Law!
-                </p>
-              </div>
-              <img src='images/rudyTitle.png' className='profilePic animated infinite bounce delay-2s"'/>
+            <Flippy
+                flipOnHover={false} // default false
+                flipOnClick={true} // default false
+                flipDirection="horizontal" // horizontal or vertical
+                ref={(r) => this.flippy = r} // to use toggle method like this.flippy.toggle()
+                // if you pass isFlipped prop component will be controlled component.
+                // and other props, which will go to div
+               /// these are optional style, it is not necessary
+            >
 
+            <FrontSide>
+                <img src='images/rudyTitle.png' ref={this.backCard} className='profilePic animated infinite bounce delay-2s"' onClick={this.showBack}/>
+              </FrontSide>
+              <BackSide style={{ backgroundColor: '#175852'}}>
+                <div className='slideText' ref={this.frontCard} onClick={this.showFront}>
+                  <h1>RUDY</h1>
+                  <p>Digital Dungeon is a Dungeons and Dragons 5th edition actual-play
+                    podcast that attempts to entertain through high-fantasy hi-jinx in the
+                    digital world of Aoll. Join us as we hit the squared circle with wrestling
+                    extarodinare Holy Hezrou, hit the books with the ever-studious mage-in-training
+                    Rudy Cobblehead, and hit the courtroom with Gad, Tortle Attorney-at-Law!
+                  </p>
+                </div>
+                </BackSide>
+              </Flippy>
             </div>
           </ScrollAnimation>
           <ScrollAnimation delay={400} animateIn="tada" duration={2}>
-            <div className='slide' id='GG'>
-              <FlipCard
-                        disabled={true}
-                        flipped={this.state.isFlipped}
-                        onFlip={this.handleOnFlip}
-                        onKeyDown={this.handleKeyDown}
-                      >
-                <img src='images/gadTitle.png' className='profilePic animated infinite bounce delay-2s"' onClick={this.showBack}/>
+
+
+            <div className='slide cardFront' id='GG'>
+            <Flippy
+                flipOnHover={false} // default false
+                flipOnClick={true} // default false
+                flipDirection="horizontal" // horizontal or vertical
+                ref={(r) => this.flippy = r} // to use toggle method like this.flippy.toggle()
+                // if you pass isFlipped prop component will be controlled component.
+                // and other props, which will go to div
+               /// these are optional style, it is not necessary
+            >
+
+            <FrontSide>
+                <img src='images/gadTitle.png' ref={this.backCard} className='profilePic animated infinite bounce delay-2s"' onClick={this.showBack}/>
+              </FrontSide>
+              <BackSide style={{ backgroundColor: '#175852'}}>
                 <div className='slideText' ref={this.frontCard} onClick={this.showFront}>
                   <h1>Gad</h1>
                   <p>Digital Dungeon is a Dungeons and Dragons 5th edition actual-play
@@ -101,7 +108,8 @@ class Characters extends Component {
                     Rudy Cobblehead, and hit the courtroom with Gad, Tortle Attorney-at-Law!
                   </p>
                 </div>
-              </FlipCard>
+                </BackSide>
+              </Flippy>
             </div>
           </ScrollAnimation>
         </div>
