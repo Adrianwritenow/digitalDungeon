6ebfc92b5cebe0773efe82bd03b63a8ecdca6031
Adrian Rodriguez <adrianwritenow@gmail.com>
2018-11-04 12:09:20 -0500

    card flipps but reverse doesnt work

--- src/components/characters/index.js
+++ src/components/characters/index.js
@@ -2,9 +2,47 @@ import React, { Component } from 'react';
 import '../../App.css';
 import "animate.css/animate.min.css";
 import ScrollAnimation from 'react-animate-on-scroll';
+import FlipCard from 'react-flipcard-2';
+
 
 
 class Characters extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    isFlipped:false
+    };
+
+    this.frontCard = React.createRef();
+
+
+      this.showFront = this.showFront.bind(this);
+      this.showBack = this.showBack.bind(this);
+      this.handleOnFlip = this.handleOnFlip.bind(this);
+
+  }
+
+
+
+  showBack() {
+    this.setState({
+      isFlipped: true
+    });
+  }
+
+  showFront() {
+    this.setState({
+      isFlipped: false
+    });
+  }
+
+  handleOnFlip(flipped) {
+   if (flipped) {
+     this.frontCard.current.focus();
+   }
+ }
+
+
   render() {
 
     return (
@@ -47,16 +85,23 @@ class Characters extends Component {
           </ScrollAnimation>
           <ScrollAnimation delay={400} animateIn="tada" duration={2}>
             <div className='slide' id='GG'>
-              <div className='slideText'>
-                <h1>Gad</h1>
-                <p>Digital Dungeon is a Dungeons and Dragons 5th edition actual-play
-                  podcast that attempts to entertain through high-fantasy hi-jinx in the
-                  digital world of Aoll. Join us as we hit the squared circle with wrestling
-                  extarodinare Holy Hezrou, hit the books with the ever-studious mage-in-training
-                  Rudy Cobblehead, and hit the courtroom with Gad, Tortle Attorney-at-Law!
-                </p>
-              </div>
-                <img src='images/gadTitle.png' className='profilePic animated infinite bounce delay-2s"'/>
+              <FlipCard
+                        disabled={true}
+                        flipped={this.state.isFlipped}
+                        onFlip={this.handleOnFlip}
+                        onKeyDown={this.handleKeyDown}
+                      >
+                <img src='images/gadTitle.png' className='profilePic animated infinite bounce delay-2s"' onClick={this.showBack}/>
+                <div className='slideText' ref={this.frontCard} onClick={this.showFront}>
+                  <h1>Gad</h1>
+                  <p>Digital Dungeon is a Dungeons and Dragons 5th edition actual-play
+                    podcast that attempts to entertain through high-fantasy hi-jinx in the
+                    digital world of Aoll. Join us as we hit the squared circle with wrestling
+                    extarodinare Holy Hezrou, hit the books with the ever-studious mage-in-training
+                    Rudy Cobblehead, and hit the courtroom with Gad, Tortle Attorney-at-Law!
+                  </p>
+                </div>
+              </FlipCard>
             </div>
           </ScrollAnimation>
         </div>
